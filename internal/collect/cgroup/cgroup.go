62f23da0c05b33d1e6fd93d6036164f2d58629ed
Scott Brant <smidley@gmail.com>
2026-08-25 22:08:10 -0700

    fix(cgroup): resolve container ids from cgroupns-relativized paths

--- internal/collect/cgroup/cgroup.go
+++ internal/collect/cgroup/cgroup.go
@@ -5,10 +5,14 @@ import "regexp"
 
 var idRe = regexp.MustCompile(`(?:docker[/-])([0-9a-f]{64})`)
 
+var bareIDRe = regexp.MustCompile(`/([0-9a-f]{64})(?:/|\n|$)`)
+
 func ContainerID(content string) (string, bool) {
-	m := idRe.FindStringSubmatch(content)
-	if m == nil {
-		return "", false
+	if m := idRe.FindStringSubmatch(content); m != nil {
+		return m[1], true
 	}
-	return m[1], true
+	if m := bareIDRe.FindStringSubmatch(content); m != nil {
+		return m[1], true
+	}
+	return "", false
 }
